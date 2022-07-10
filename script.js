5797ac389eb36fdc5815c02e4d97d1f6b900273c
[derealOG] <[derealOG001@gmail.com]>
2022-07-10 03:01:36 -1000

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
